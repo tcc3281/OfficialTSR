6c8ccdcc85d5fcda696f11754440d8d0b39ece68
Chiến Trần <116490257+tcc3281@users.noreply.github.com>
2025-04-13 00:47:38 +0700

    update api and camera

--- app/src/main/java/com/example/officialtsr/api/RetrofitClient.java
+++ app/src/main/java/com/example/officialtsr/api/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.1.15:8000";
+    private static final String BASE_URL = "http://3.107.235.182:8000";
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
